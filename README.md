4e5acb1782b39109212eccb46f258b18e64209df
Khalid Hussein <husseinkhalid043@gmail.com>
2024-12-13 05:21:33 +0000

    Initial commit - Added all files for inet_4031_adduser_script

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # inet_4031_adduser_script
+# Users script 
+
+## Program Description
+This script will add users to our Linux virutal machine
+
+## Program Operation
+Follow these steps
+
+1. Clone repo file you Created using the git clone command with the url
+
+2. Create pyton file to add new user
+
+3. Test code
+
---
+++ create-users.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import os
+import re
+import sys
+
+def main():
+  for line in sys.stdin:
+    match = re.match("^#",line)
+    fields = line.strip().split(':')
+
+    if match or len(fields) !=5:
+      continue
+    username = fields[0]
+    password = fields[1]
+    gecos = "%s %s,,,"% (fields[3],fields[2])
+    groups = fields[4].split(',')
+    print("==> Creating account for %s..." % (username))
+    cmd = "/bin/echo -ne '%s\n%s' | /user/bin/sudo /user/bin/passwd %s" % (password,password,username)
+    #print cmd
+    ##os.system(cmd)
+
+
+    for group in groups:
+      if group != '-' :
+        print("==> Assigning %s to the %s group..." % (username,group))
+        cmd = "/user/sbin/adduser %s %s" % (username,group)
+        #print cmd
+	#os.system(cmd)
+
+if __name__ == '__main__':
+    main()
